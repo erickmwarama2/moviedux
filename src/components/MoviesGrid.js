aeb04c58493778e7dd96fca51f041d38e9fc0dab
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-05 21:45:42 +0300

    add search functionality

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -4,26 +4,42 @@ import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const onSearchInput = (event) => {
+    setSearchTerm(event.target.value);
+  };
 
   useEffect(() => {
     async function fetchMovies() {
       try {
         const response = await fetch("movies.json");
         const movies = await response.json();
-        setMovies(movies);
+        const filteredMovies = movies.filter((movie) =>
+          movie.title.toLowerCase().includes(searchTerm)
+        );
+        setMovies(filteredMovies);
       } catch (error) {
         console.log(error);
       }
     }
 
     fetchMovies();
-  }, []);
+  }, [searchTerm]);
 
   return (
-    <div className="movies-grid">
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
+    <div>
+      <input
+        type="text"
+        placeholder="Search movies..."
+        className="search-input"
+        onChange={onSearchInput}
+      />
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+      </div>
     </div>
   );
 }
