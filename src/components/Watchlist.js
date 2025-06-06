26a8a03b02d036d91f70ab10aca87afa3bf488ef
Erick Mutwiri <e.mutwiri@amplifytrading.com>
2025-06-06 10:47:20 +0300

    create watchlist component

--- src/components/Watchlist.js
+++ src/components/Watchlist.js
@@ -1,9 +1,23 @@
 import "../styles.css";
+import MovieCard from "./MovieCard";
 
 export default function Watchlist({ movies, watchlist, toggleWatchlist }) {
   return (
     <div>
       <h1 className="title"> Your Watchlist </h1>
+      <div className="watchlist">
+        {watchlist.map((id) => {
+          const movie = movies.find((movie) => movie.id === id);
+          return (
+            <MovieCard
+              movie={movie}
+              key={id}
+              isWatchlisted={true}
+              toggleWatchlist={toggleWatchlist}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
